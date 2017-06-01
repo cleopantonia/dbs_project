00f706d679f4362410c6dbdf7a44a7022b334964
EvaFriederike <evus@zedat.fu-berlin.de>
2017-06-02 00:41:41 +0200

    Update Datenbereinigung.py
    Umlaute entfernt

--- Datenbereinigung.py
+++ Datenbereinigung.py
@@ -20,11 +20,11 @@ print(df_new.head())
 
 # hier war mal: ersetze '%' durch den String 'percent', da Zeichen in sql einen wildcard character darstellt
 #               df_new['text'] = df_new['text'].str.replace('%',' percent')
-# NICHT NÖTIG, DA DURCH DIE RICHTIGE FORMATIERUNG (Mit "...string")
+# NICHT NOETIG, DA DURCH DIE RICHTIGE FORMATIERUNG (Mit "...string")
 # IN DER SQL-QUERY DAS % NICHT ALS WILDCARD ERKANNT WIRD
 
-# ersetze Apostrophe aus Tweet-Texten durch doppelte Apostrophe, um sql-Probleme von vornherein auszuschließen
-# evtl. Interferenzen zwischen sql und Hochkommata o.Ä. werden durch richtige String-Formatierung in queries verhindert
+# ersetze Apostrophe aus Tweet-Texten durch doppelte Apostrophe, um sql-Probleme von vornherein auszuschliessen
+# evtl. Interferenzen zwischen sql und Hochkommata o.Ae. werden durch richtige String-Formatierung in queries verhindert
 df_new['text'] = df_new['text'].str.replace("'", "''")
 
 # erster Test, ob es geklappt hat
@@ -32,7 +32,7 @@ print(df_new.head(10))
 # zweiter Test, ob es geklappt hat
 print(df_new.loc[[13]])
 
-# überprüfe, ob alle Spalten, die keinen Null-Eintrag haben dürfen, auch wirklich keinen haben
+# ueberpruefe, ob alle Spalten, die keinen Null-Eintrag haben duerfen, auch wirklich keinen haben
 # hier: handle,text,time,retweet_count,favorite_count,source_url
 # gib alle Reihen aus, in denen eine Spalte gegen diese Anforderung widerspricht
 assert any(~df_new['handle'].isnull())
@@ -42,8 +42,8 @@ assert any(~df_new['retweet_count'].isnull())
 assert any(~df_new['favorite_count'].isnull())
 assert any(~df_new['source_url'].isnull())
 
-# will überprüfen, ob Datentypen jeder Spalte einheitlich sind
-# gib Datentypen für jede Spalte aus
+# will ueberpruefen, ob Datentypen jeder Spalte einheitlich sind
+# gib Datentypen fuer jede Spalte aus
 print(df_new.dtypes)
 
 # check, ob jede Spalte den jeweiligen Datentyp bedient und gib einen Error aus, falls eine Spalte einen Eintrag mit
@@ -64,15 +64,15 @@ print(df_new.shape)
 
 # entferne Duplikate: A und B seien Duplikate <=> A['handle']==B['handle'] und A['time']==B['time']
 # unter der Annahme, dass nicht zwei verschiedene Tweets vom selben Account
-# zur exakt selben Zeit gepostet werden können
+# zur exakt selben Zeit gepostet werden koennen
 df_new.drop_duplicates(subset=df_new[[0,2]], keep='first', inplace=False).head(n=10)
 
-# Prüfe Zeilenanzahl nach Duplikaten-Entfernung
-# Anzahl der entries unverändert, also existierten gar keine Duplikate
+# Pruefe Zeilenanzahl nach Duplikaten-Entfernung
+# Anzahl der entries unveraendert, also existierten gar keine Duplikate
 print(df_new.shape)
 
 # konvertiere Datentyp von time(object) in den Datentyp datetime um
-# wichtig für das Laden der Daten in die Datenbank (time hat in sql den Typ timestamp)
+# wichtig fuer das Laden der Daten in die Datenbank (time hat in sql den Typ timestamp)
 pd.to_datetime(df_new.time)
 df_new.time = pd.to_datetime(df_new.time)
 
@@ -80,14 +80,14 @@ df_new.time = pd.to_datetime(df_new.time)
 df_new.columns = ['handle','inhalt','zeit','in_reply_to','retweet_count','favorite_count','source_url']
 print(df_new.head())
 
-# ersetze die Null-Einträge bei in_reply_to durch einen leeren String
+# ersetze die Null-Eintraege bei in_reply_to durch einen leeren String
 df_new.in_reply_to.fillna('')
 
 # speichere gereinigten Datentyp
 #df_new.to_csv('cleansed_dataframe.csv')
 
 #from sqlalchemy import create_engine
-# Lade Datensatz in die Datenbank election in die Realtion twitter über die erstellte connection:
+# Lade Datensatz in die Datenbank election in die Realtion twitter ueber die erstellte connection:
 #engine = create_engine('postgres://postgres:postgres@localhost:5432/election')
 #df_new.to_sql(name='tweet',con=engine ,index_label='t_id', schema='e_schema',if_exists='append')
 
@@ -101,25 +101,25 @@ df_help = df_new[df_new['inhalt'].str.contains("#")]
 hash_tag['name'] = df_help['inhalt']
 print(hash_tag)
 
-# Index stellt später Hashtag_ID => zwei Hashtags in einem Tweet müssen verschiedene IDs haben, deshalb einen
+# Index stellt spaeter Hashtag_ID => zwei Hashtags in einem Tweet muessen verschiedene IDs haben, deshalb einen
 # neuen fortlaufenden Index erstellen
-# Überschreibe den Index
+# Ueberschreibe den Index
 hash_tag.reset_index(drop=True)
 
 import re
 
 # finde alle Hashtags mit den folgenden Eigenschaften:
-# String beginnt direkt nach einem Rautezeichen und enthält NUR Groß-/Kleinbuchstaben und Zahlen,
+# String beginnt direkt nach einem Rautezeichen und enthaelt NUR Grss-/Kleinbuchstaben und Zahlen,
 # keine Sonderzeichen und kein Leerzeichen
 # das Rautezeichen selbst soll nicht gespeichert werden
 pat = re.compile(r"#([a-zA-Z0-9]+)")
 # gib gleich tags aus und zeige leere Liste in letzter Zeile
 tags = []
-# speichere das Ergebnis für jeden Zell-Eintrag in einer Liste, da ein Tweet mehrere Hashtags haben kann
+# speichere das Ergebnis fuer jeden Zell-Eintrag in einer Liste, da ein Tweet mehrere Hashtags haben kann
 for i in hash_tag['name']:
     tags += [pat.findall(i)]
 
-# erstelle ein leeres Set und fülle es mit allen Hashtags aus der Liste tags
+# erstelle ein leeres Set und fuelle es mit allen Hashtags aus der Liste tags
 # sets enthalten keine Duplikate
 set_tags = set()
 for l in tags:
@@ -129,12 +129,12 @@ for l in tags:
 # Gib Menge der eindeutigen Hashtags aus
 len(set_tags)
 
-# erstelle neuen data frame und fülle name-column mit den eindeutigen Hashtags
+# erstelle neuen data frame und fuelle name-column mit den eindeutigen Hashtags
 df_tags = pd.DataFrame(columns=['name','abs_hauf'])
 df_tags['name'] = list(set_tags)
 print(df_tags)
 
-# definiere Zählfunktion für absolute Häufigkeiten
+# definiere Zaehlfunktion fuer absolute Haeufigkeiten
 def count_occurrences(hashtag):
     count = 0
     for l in tags:
@@ -142,7 +142,7 @@ def count_occurrences(hashtag):
             count += 1
     return count
 
-# zähle für jeden Hashtag im set die absolute Häufigkeit in der Liste tags (nicht-eindeutigen Hashtag-Liste)
+# zaehle fuer jeden Hashtag im set die absolute Haeufigkeit in der Liste tags (nicht-eindeutigen Hashtag-Liste)
 df_tags.abs_hauf = df_tags.name.apply(count_occurrences)
 print(df_tags)
 
@@ -153,7 +153,7 @@ print(df_tags)
 #engine2 = create_engine('postgres://postgres:postgres@localhost:5432/election')
 #df_tags.to_sql(name='hashtag',con=engine2 ,index_label='h_id', schema='e_schema',if_exists='append')
 
-# erstelle neues data frame für enthaelt-table (e_schema) => Zieltabelle
+# erstelle neues data frame fuer enthaelt-table (e_schema) => Zieltabelle
 df_contains = pd.DataFrame(columns=['f_t_id','f_h_id'])
 
 # erstelle dataframe hilfe2 mit den tweet-IDs und der jeweiligen Liste von Hashtags
@@ -166,13 +166,13 @@ hilfe2['inhalt'] = tags
 # wurde. In der hilfe2-Tabelle steht daher im entsprechenden Eintrag nur eine leere Liste
 print(hilfe2)
 
-# füge für jeden Hashtag eines Tweets die Tweet-ID in die Liste indexe ein
+# fuege fuer jeden Hashtag eines Tweets die Tweet-ID in die Liste indexe ein
 indexe = []
 for idx, x in hilfe2['inhalt'].iteritems():
     for k in x:
         indexe.append(idx)
 
-# füge für jeden Hashtag in einem Tweet die entsprechende Hashtag-ID aus df_tags in die Liste indices
+# fuege fuer jeden Hashtag in einem Tweet die entsprechende Hashtag-ID aus df_tags in die Liste indices
 # indices ist Liste von Listen, da Hashtags in hilfe2['inhalt'] ebenfalls als Listen in einer Liste gespeichert sind
 indices = []
 for l in hilfe2['inhalt']:
@@ -191,7 +191,7 @@ print(df_contains)
 #from sqlalchemy import create_engine
 #engine3 = create_engine('postgres://postgres:postgres@localhost:5432/election')
 
-# die Daten aus df_contains können nicht in die Datenbank in die Relation enthaelt importiert werden, da im Tweet mit der ID
+# die Daten aus df_contains koennen nicht in die Datenbank in die Relation enthaelt importiert werden, da im Tweet mit der ID
 # 4969 zweimal der Hashtag #Trump vorkommt
-# Löschen des zweiten Eintrags der Doppelung würde alle anderen tables verfälschen (z.B. abs_hauf in e_schema.hashtag)
+# Loeschen des zweiten Eintrags der Doppelung wuerde alle anderen tables verfaelschen (z.B. abs_hauf in e_schema.hashtag)
 #df_contains.to_sql(name='enthaelt',con=engine3 ,index=False, schema='e_schema',if_exists='append')
